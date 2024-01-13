1a88eaf2e26c89e4f454ec3059329fa5fbd98e8a
Lê Xuân Nhật <54104903+rt-kc@users.noreply.github.com>
2024-01-13 17:46:18 +0700

    fix

--- main/src/components/Header.vue
+++ main/src/components/Header.vue
@@ -6,13 +6,15 @@
         <v-app-bar-title>
             Application
         </v-app-bar-title>
-        <v-text-field clearable=""
-                      class="custom-padding"
-                      rounded
-                      bg-color="#151718"
-                      label="Search"
-                      prepend-inner-icon="mdi-magnify"
-                      variant="solo-filled"
+        <v-text-field
+            clearable="true"
+            class="pt-6"
+            rounded
+            bg-color="#151718"
+            label="Search"
+            prepend-inner-icon="mdi-magnify"
+            variant="solo-filled"
+            style="max-width: 250px;"
         ></v-text-field>
         <v-list-item
             prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
@@ -22,7 +24,5 @@
 </template>
 
 <style scoped>
-    .custom-padding {
-        padding-top: 25px;
-    }
+
 </style>
