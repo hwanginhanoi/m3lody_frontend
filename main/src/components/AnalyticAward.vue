bcac3e4e5c064bbe8dd7714b4cb484a6ad89be9d
Luu Tuan Hoang <hoanglt0104@gmail.com>
2024-01-16 11:53:11 +0700

    Dashboard

---
+++ main/src/components/AnalyticAward.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+    <v-card class="position-relative">
+        <v-card-text>
+            <div class="mb-2">
+                <h5 class="text-h5">
+                    Ditconme <span class="text-high-emphasis">🥵</span>
+                </h5>
+                <div class="text-body-1">
+                    Ngu nhu cac
+                </div>
+            </div>
+            <h4 class="text-h4">
+                Rac
+            </h4>
+            <div class="text-body-1 mb-2">
+                Ok <span class="text-high-emphasis">👍🏻</span>
+            </div>
+            <v-btn size="small">
+                Mmb
+            </v-btn>
+        </v-card-text>
+    </v-card>
+</template>
+
+<style>
+
+
+</style>
--- main/src/views/Index.vue
+++ main/src/views/Index.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
 
+import AnalyticAward from "../components/AnalyticAward.vue";
+import AnalyticTransaction from "../components/AnalyticTransaction.vue";
 </script>
 
 <template>
-
+    <v-main>
+        <v-row>
+            <v-col cols="12" md="4">
+                <v-card>
+                    <AnalyticAward></AnalyticAward>
+                </v-card>
+            </v-col>
+            <v-col cols="12" md="8">
+                <v-card>
+                    <AnalyticTransaction></AnalyticTransaction>
+                </v-card>
+            </v-col>
+            <v-col cols="12" md ="4">
+                <v-card>
+                    ewffewfew
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-main>
 </template>
 
 <style scoped>
