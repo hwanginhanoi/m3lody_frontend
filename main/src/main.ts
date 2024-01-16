bd6de01cd3882abb72e716fe9d6712b448e94f0f
Lê Xuân Nhật <tori.tuan@gmail.com>
2024-01-16 13:32:44 +0700

    add vuetify.ts file to plugins

--- main/src/main.ts
+++ main/src/main.ts
@@ -3,16 +3,6 @@ import './style.css'
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './plugins/router.ts';
-
-
-// Vuetify
-import 'vuetify/styles'
-import {createVuetify} from 'vuetify'
-
-const vuetify = createVuetify({
-    theme: {
-        defaultTheme: 'dark',
-    },
-})
+import vuetify from "./plugins/vuetify.ts";
 
 createApp(App).use(vuetify).use(router).mount('#app');
---
+++ main/src/plugins/vuetify.ts
@@ -0,0 +1,10 @@
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+
+const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'dark',
+    },
+})
+
+export default vuetify;
