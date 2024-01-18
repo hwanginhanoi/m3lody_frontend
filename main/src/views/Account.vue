dc3ead37519532fa9f20d7d8376040d52096fd3a
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-01-18 23:28:26 +0700

    u

--- main/src/views/Account.vue
+++ main/src/views/Account.vue
@@ -214,154 +214,164 @@ const tab = ref('1');
                         </v-card>
                     </v-container>
                 </v-window-item>
-                <v-window-item :value="2">
-                    <v-card class=" pa-10 backgroundgra my-16 rounded-xl" elevation="20"
-                            width="90%"
-                    >
-                        <v-card-text class="pt-2"><h3>Account Details</h3></v-card-text>
-                        <v-card-text class="pt-2">
-                            <v-row>
-                                <v-col :cols="mdAndUp?'':12">
-                                    <v-row>
-                                        <v-col>
-                                            <v-text-field
-                                                v-model="security.currentpassword"
-                                                label="Current Password"
-                                                type="text"
-                                                clearable=""
-                                                rounded
-                                                variant="outlined"
-                                                color="#d777ed"
-                                            ></v-text-field>
-                                            <v-text-field
-                                                v-model="security.newpassword"
-                                                label="New Password"
-                                                type="text"
-                                                clearable=""
-                                                rounded
-                                                variant="outlined"
-                                                color="#d777ed"
-                                                ></v-text-field>
+                <v-window-item
+                    v-for="n in 3"
+                    :key="n"
+                    :value="2">
+                    <v-container class="d-flex justify-center ">
 
-                                        </v-col>
-                                    </v-row>
-                                </v-col>
-                                <v-col>
-                                    <v-row>
-                                        <v-col>
-                                            <v-text-field
-                                                v-model="security.confirmpassword"
-                                                label="Confirm Password"
-                                                type="text"
-                                                clearable=""
-                                                rounded
-                                                variant="outlined"
-                                                color="#d777ed"
-                                            ></v-text-field>
-                                        </v-col>
-                                    </v-row>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-card-text class="ml-6">
-                                    <p>Password Requirement</p>
-                                    <ul>
-                                        <li>Minimum 8 characters long - the more, the better</li>
-                                        <li>At least one lowercase character</li>
-                                        <li>At least one number, symbol, or whitespace character</li>
-                                    </ul>
-                                </v-card-text>
-                            </v-row>
-                            <v-row>
-                                <v-card-text>
-                                    <v-btn class="ml-2" color="purple" max-width="150">Save Changes</v-btn>
-                                    <v-btn class="ml-2" color="white" max-width="150">Reset</v-btn>
-                                </v-card-text>
-                            </v-row>
-                        </v-card-text>
-                    </v-card>
+                        <v-card class=" pa-10 backgroundgra my-16 rounded-xl" elevation="20"
+                                width="900%"
+                        >
+                            <v-card-text class="pt-2"><h3>Account Details</h3></v-card-text>
+                            <v-card-text class="pt-2">
+                                <v-row>
+                                    <v-col :cols="mdAndUp?'':12">
+                                        <v-row>
+                                            <v-col>
+                                                <v-text-field
+                                                    v-model="security.currentpassword"
+                                                    label="Current Password"
+                                                    type="text"
+                                                    clearable=""
+                                                    rounded
+                                                    variant="outlined"
+                                                    color="#d777ed"
+                                                ></v-text-field>
+                                                <v-text-field
+                                                    v-model="security.newpassword"
+                                                    label="New Password"
+                                                    type="text"
+                                                    clearable=""
+                                                    rounded
+                                                    variant="outlined"
+                                                    color="#d777ed"
+                                                    ></v-text-field>
+
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+                                    <v-col>
+                                        <v-row>
+                                            <v-col>
+                                                <v-text-field
+                                                    v-model="security.confirmpassword"
+                                                    label="Confirm Password"
+                                                    type="text"
+                                                    clearable=""
+                                                    rounded
+                                                    variant="outlined"
+                                                    color="#d777ed"
+                                                ></v-text-field>
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-card-text class="ml-6">
+                                        <p>Password Requirement</p>
+                                        <ul>
+                                            <li>Minimum 8 characters long - the more, the better</li>
+                                            <li>At least one lowercase character</li>
+                                            <li>At least one number, symbol, or whitespace character</li>
+                                        </ul>
+                                    </v-card-text>
+                                </v-row>
+                                <v-row>
+                                    <v-card-text>
+                                        <v-btn class="ml-2" color="purple" max-width="150">Save Changes</v-btn>
+                                        <v-btn class="ml-2" color="white" max-width="150">Reset</v-btn>
+                                    </v-card-text>
+                                </v-row>
+                            </v-card-text>
+                        </v-card>
+                    </v-container>
                 </v-window-item>
 
                 <v-window-item :value="3">
-                    <v-card class=" pa-10 backgroundgra my-16 rounded-xl" elevation="20"
-                            width="90%"
-                    >
-                        <v-card-text class="pt-2"><h3>Payment Details</h3></v-card-text>
-                        <v-card-text class="pt-2">
-                            <v-row>
-                                <v-col :cols="mdAndUp?'':12">
-                                    <v-row>
-                                        <v-col>
-                                            <v-text-field
-                                                v-model="paymentDetails.cardholdername"
-                                                label="Card Holder Name"
-                                                type="text"
-                                                clearable=""
-                                                rounded
-                                                variant="outlined"
-                                                color="#d777ed"
-                                            ></v-text-field>
-                                            <v-text-field
-                                                v-model="paymentDetails.cardnumber"
-                                                label="Card Number"
-                                                type="text"
-                                                clearable=""
-                                                rounded
-                                                variant="outlined"
-                                                color="#d777ed"
-                                            ></v-text-field>
-                                            <v-text-field
-                                                v-model="paymentDetails.cvv"
-                                                label="CVV"
-                                                type="text"
-                                                clearable=""
-                                                rounded
-                                                variant="outlined"
-                                                color="#d777ed"
-                                            ></v-text-field>
+                    <v-container class="d-flex justify-center ">
 
-                                        </v-col>
-                                    </v-row>
-                                </v-col>
-                                <v-col>
-                                    <v-row>
-                                        <v-col>
-                                            <v-text-field
-                                                v-model="security.confirmpassword"
-                                                label="Confirm Password"
-                                                type="text"
-                                                clearable=""
-                                                rounded
-                                                variant="outlined"
-                                                color="#d777ed"
-                                            ></v-text-field>
-                                            <v-text-field
-                                                v-model="security.expirationdate"
-                                                label="Expiration Date"
-                                                type="text"
-                                                clearable=""
-                                                rounded
-                                                variant="outlined"
-                                                color="#d777ed"
-                                            ></v-text-field>
-                                        </v-col>
-                                    </v-row>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-card-text class="ml-2">
-                                    <v-checkbox label="Agree to our Terms and Conditions ">
-                                    </v-checkbox>
-                                </v-card-text>
-                            </v-row>
-                            <v-row>
-                                <v-card-text>
-                                    <v-btn class="ml-2" color="purple" max-width="150">Save Changes</v-btn>
-                                    <v-btn class="ml-2" color="white" max-width="150">Reset</v-btn>
-                                </v-card-text>
-                            </v-row>
-                        </v-card-text>
-                    </v-card>
+                        <v-card class=" pa-10 backgroundgra my-16 rounded-xl" elevation="20"
+                                width="90%"
+                        >
+                            <v-card-text class="pt-2"><h3>Payment Details</h3></v-card-text>
+                            <v-card-text class="pt-2">
+                                <v-row>
+                                    <v-col :cols="mdAndUp?'':12">
+                                        <v-row>
+                                            <v-col>
+                                                <v-text-field
+                                                    v-model="paymentDetails.cardholdername"
+                                                    label="Card Holder Name"
+                                                    type="text"
+                                                    clearable=""
+                                                    rounded
+                                                    variant="outlined"
+                                                    color="#d777ed"
+                                                ></v-text-field>
+                                                <v-text-field
+                                                    v-model="paymentDetails.cardnumber"
+                                                    label="Card Number"
+                                                    type="text"
+                                                    clearable=""
+                                                    rounded
+                                                    variant="outlined"
+                                                    color="#d777ed"
+                                                ></v-text-field>
+                                                <v-text-field
+                                                    v-model="paymentDetails.cvv"
+                                                    label="CVV"
+                                                    type="text"
+                                                    clearable=""
+                                                    rounded
+                                                    variant="outlined"
+                                                    color="#d777ed"
+                                                ></v-text-field>
+
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+                                    <v-col>
+                                        <v-row>
+                                            <v-col>
+                                                <v-text-field
+                                                    v-model="security.confirmpassword"
+                                                    label="Confirm Password"
+                                                    type="text"
+                                                    clearable=""
+                                                    rounded
+                                                    variant="outlined"
+                                                    color="#d777ed"
+                                                ></v-text-field>
+                                                <v-text-field
+                                                    v-model="security.expirationdate"
+                                                    label="Expiration Date"
+                                                    type="text"
+                                                    clearable=""
+                                                    rounded
+                                                    variant="outlined"
+                                                    color="#d777ed"
+                                                ></v-text-field>
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-card-text class="ml-2">
+                                        <v-checkbox label="Agree to our Terms and Conditions ">
+                                        </v-checkbox>
+                                    </v-card-text>
+                                </v-row>
+                                <v-row>
+                                    <v-card-text>
+                                        <v-btn class="ml-2" color="purple" max-width="150">Save Changes</v-btn>
+                                        <v-btn class="ml-2" color="white" max-width="150">Reset</v-btn>
+                                    </v-card-text>
+                                </v-row>
+                            </v-card-text>
+                        </v-card>
+                    </v-container>
+
                 </v-window-item>
             </v-window>
         </v-card>
