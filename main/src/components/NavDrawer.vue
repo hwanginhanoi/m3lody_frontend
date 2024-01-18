9560d31c81b1755fca795369d7a32499316100ab
Luu Tuan Hoang <hoanglt0104@gmail.com>
2024-01-18 10:20:15 +0700

    Update NavDrawer.vue

--- main/src/components/NavDrawer.vue
+++ main/src/components/NavDrawer.vue
@@ -21,7 +21,7 @@ let avatar : string = "https://randomuser.me/api/portraits/women/85.jpg"
 <template>
     <v-navigation-drawer
         v-if="mdAndUp"
-        width="210"
+        width="260"
         color="nav-drawer"
         opacity='0.4'
         :rail="mdAndUp ? rail : false"
@@ -29,7 +29,7 @@ let avatar : string = "https://randomuser.me/api/portraits/women/85.jpg"
     >
         <div class="d-flex">
             <v-app-bar-nav-icon @click.stop="rail=!rail" class="ml-1 mt-6"></v-app-bar-nav-icon>
-            <v-img v-if="mdAndUp && !rail" src="src/assets/trade-dark.png" max-height="30" class="mt-8 mr-2"
+            <v-img v-if="mdAndUp && !rail" src="src/assets/trade-dark.png" max-height="30" class="mt-8 mr-9"
             ></v-img>
         </div>
         <v-list density="compact" class="d-flex flex-column h100 rounded-list mt-5">
@@ -70,7 +70,7 @@ let avatar : string = "https://randomuser.me/api/portraits/women/85.jpg"
     <v-navigation-drawer
         v-model="drawer"
         v-if="!mdAndUp"
-        width="210"
+        width="260"
         color="nav-drawer"
         opacity='0.4'
     >
@@ -78,13 +78,19 @@ let avatar : string = "https://randomuser.me/api/portraits/women/85.jpg"
             <v-img src="src/assets/trade-dark.png" max-height="30" class="mt-8"
             ></v-img>
         </div>
-        <v-list density="compact" class="d-flex flex-column h100 rounded-list">
-            <v-list-item to="/index" prepend-icon="mdi-dots-grid" title="Dashboard" value="dashboard" class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
-            <v-list-item to="/account" prepend-icon="mdi-account-multiple" title="User" value="account" class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
-            <v-list-item to="/watchlist" prepend-icon="mdi-eye" title="Watchlist" value="watchlist" class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
-            <v-list-item to="/wallet" prepend-icon="mdi-wallet" title="Wallet" value="wallet" class="py-2 mb-4 mr-2 customPrepend"></v-list-item>
-            <v-list-item to="/marketplace" prepend-icon="mdi-store" title="Marketplace" value="marketplace" class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
-            <v-list-item to="/exchange" prepend-icon="mdi-cash-multiple" title="Exchange" value="exchange" class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
+        <v-list density="compact" class="d-flex flex-column h100 rounded-list mt-5">
+            <v-list-item to="/index" prepend-icon="mdi-dots-grid" title="Dashboard" value="dashboard"
+                         class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
+            <v-list-item to="/account" prepend-icon="mdi-account-multiple" title="User" value="account"
+                         class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
+            <v-list-item to="/watchlist" prepend-icon="mdi-eye" title="Watchlist" value="watchlist"
+                         class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
+            <v-list-item to="/wallet" prepend-icon="mdi-wallet" title="Wallet" value="wallet"
+                         class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
+            <v-list-item to="/marketplace" prepend-icon="mdi-store" title="Marketplace" value="marketplace"
+                         class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
+            <v-list-item to="/exchange" prepend-icon="mdi-cash-multiple" title="Exchange" value="exchange"
+                         class="py-2 mb-4 mr-2 rounded-button customPrepend"></v-list-item>
         </v-list>
 
 
