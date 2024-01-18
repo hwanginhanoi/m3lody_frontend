5e5a3973dcebfe6f9146e831ce08a37f80f52e01
Luu Tuan Hoang <101512686+hwanginhanoi@users.noreply.github.com>
2024-01-18 15:58:03 +0700

    Stylewallet (#18)
    * u
    
    * margin + barchart done
    
    margin + barchart done
    
    * Update .gitignore
    
    * Update index.html
    
    * Update package.json
    
    * Update package-lock.json
    
    ---------
    
    Co-authored-by: khanhdnk <121726197+khanhdnk@users.noreply.github.com>
    Co-authored-by: Thanh Duong <clanaoetruongxe@gmail.com>
    Co-authored-by: J.K.Roflol <54104903+jkroflol-rk@users.noreply.github.com>

--- main/src/main.ts
+++ main/src/main.ts
@@ -2,12 +2,15 @@ import './style.css'
 
 import {createApp} from 'vue'
 import App from './App.vue'
+
+import VueApexCharts from "vue3-apexcharts";
 import router from './plugins/router.ts';
 import vuetify from "./plugins/vuetify.ts";
 import Particles from "@tsparticles/vue3";
 import { loadFull } from "tsparticles"
 
-createApp(App).use(vuetify).use(router).use(Particles, {
+createApp(App).use(vuetify).use(router).use(VueApexCharts).use(Particles, {
   init: async (engine: any) => {
     await loadFull(engine);
   }}).mount('#app');
+
