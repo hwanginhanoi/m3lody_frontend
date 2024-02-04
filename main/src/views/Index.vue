df80ea094418db706a7c9cacd199d7541e3d4f2e
Lê Xuân Nhật <54104903+rt-kc@users.noreply.github.com>
2024-02-04 22:26:09 +0700

    minor updates

--- main/src/views/Index.vue
+++ main/src/views/Index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 // import required modules
+import {ref} from "vue";
 import {useDisplay} from "vuetify";
 // Import Swiper styles
 import 'swiper/css/bundle';
@@ -38,7 +39,8 @@ let items = [
     {rank: 10, collection: 'Collection 10', floorPrice: 1.0, volume: 1.4},
 ];
 
-
+const model = ref(0);
+const model2 = ref(0);
 </script>
 
 <template>
@@ -47,22 +49,22 @@ let items = [
             <h1>Dashboard</h1>
             <SwiperCard/>
             <div class="d-flex justify-space-between my-10">
-                <v-btn-toggle>
-                    <v-btn>
+                <v-btn-toggle v-model="model">
+                    <v-btn value="0">
                         Trending
                     </v-btn>
-                    <v-btn>
+                    <v-btn value="1">
                         Top
                     </v-btn>
                 </v-btn-toggle>
-                <v-btn-toggle>
-                    <v-btn>
+                <v-btn-toggle v-model="model2">
+                    <v-btn value="0">
                         24h
                     </v-btn>
-                    <v-btn>
+                    <v-btn value="1">
                         7d
                     </v-btn>
-                    <v-btn>
+                    <v-btn value="2">
                         30d
                     </v-btn>
                 </v-btn-toggle>
