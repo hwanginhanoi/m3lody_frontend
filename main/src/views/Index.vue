393dd2c781d674a180b2ab6c34849fa5b3bb1397
Lê Xuân Nhật <54104903+rt-kc@users.noreply.github.com>
2024-02-05 16:11:01 +0700

    group session

--- main/src/views/Index.vue
+++ main/src/views/Index.vue
@@ -5,19 +5,50 @@ import {ref, onMounted} from 'vue';
 const particlesLoaded = async (container: any) => {
     console.log("Particles container loaded", container);
 };
+
 let target = ref(50000);
+let target2 = ref(15000);
+let target3 = ref(3000000);
 
 let transaction = ref(0);
+let sold = ref(0);
+let artists = ref(0);
 
 onMounted(() => {
     // Increase the transaction variable when the component is first loaded
-    if (target.value < 50001)
-        setInterval(increaseNumber, 0.4);
+    setInterval(increaseNumber, 7);
 });
 
 function increaseNumber() {
-    if (transaction.value < target.value) {
-        transaction.value = transaction.value + 50;
+    if (sold.value < target.value) {
+        sold.value = Math.min(sold.value + 250, target.value);
+    }
+    if (artists.value < target2.value) {
+        artists.value = Math.min(artists.value + 50, target2.value);
+    }
+    if (transaction.value < target3.value) {
+        transaction.value = Math.min(transaction.value + 10000, target3.value);
+    }
+
+    // Format the numbers when they reach the desired values
+    if (sold.value === target.value) {
+        sold.value = formatNumber(target.value);
+    }
+    if (artists.value === target2.value) {
+        artists.value = formatNumber(target2.value);
+    }
+    if (transaction.value === target3.value) {
+        transaction.value = formatNumber(target3.value);
+    }
+}
+
+function formatNumber(number) {
+    if (number >= 1000000) {
+        return (number / 1000000).toFixed(1) + 'M+';
+    } else if (number >= 1000) {
+        return (number / 1000).toFixed(1) + 'k+';
+    } else {
+        return number;
     }
 }
 
@@ -117,28 +148,50 @@ function increaseNumber() {
                 </v-col>
             </v-row>
             <v-row style="color: #7e66f9">
-                <v-col style="background-color: white;" class="d-flex justify-center py-16">
+                <v-col :style="{background: $vuetify.theme.global.current.colors.navbtn}"  class="d-flex justify-center py-16">
                     <div>
-                        <h1>3M+</h1>
+                        <h1>{{ sold }}</h1>
                         <p>NFT Sold</p>
                     </div>
 
                 </v-col>
-
-                <v-col style="background-color: black" class="d-flex justify-center py-16">
+                <v-divider vertical :thickness="2" class="border-opacity-50"></v-divider>
+                <v-col :style="{background: $vuetify.theme.global.current.colors.navbtn}"  class="d-flex justify-center py-16">
                     <div>
-                        <h1>15K+</h1>
+                        <h1>{{ artists }}</h1>
                         <p>artists</p>
                     </div>
                 </v-col>
-
-                <v-col style="background-color: white" class="d-flex justify-center py-16">
+                <v-divider vertical :thickness="2" class="border-opacity-50"></v-divider>
+                <v-col :style="{background: $vuetify.theme.global.current.colors.navbtn}"  class="d-flex justify-center py-16">
                     <div>
-                        <h1>$ {{ transaction }}</h1>
+                        <h1>${{ transaction }}</h1>
                         <p>Total Transaction</p>
                     </div>
                 </v-col>
             </v-row>
+
+            <v-row :style="{background: $vuetify.theme.global.current.colors.navbtn}" class="spaceuper">
+
+                <v-col class="mt-16" cols="12">
+                    <v-col cols="7" class=" parallelogram bg-purple-accent-4 setheight" offset="1">
+                        <div class="bars">
+                            <h2><v-icon>mdi-credit-card-check</v-icon>Fast Transactions</h2>
+                            <p>Experience top-of-the-line efficiency with swift NFT transactions, ensuring seamless transfers of digital assets across blockchain networks with unparalleled speed. </p>
+                        </div>
+                    </v-col>
+                </v-col>
+                <v-col cols="12" class="mt-4">
+                    <v-col cols="7" offset="4" class="setheight parallelogram bg-purple-accent-4">
+                        <div class="bars">
+                            <h2><v-icon>mdi-link-lock</v-icon>Secure Blockchain System</h2>
+                            <p>Benefit from the top-of-the-line, advanced blockchain system, ensuring state-of-the-art security for all digital assets and transactions.</p>
+                        </div>
+                    </v-col>
+                </v-col>
+            </v-row>
+
+
         </v-container>
     </v-main>
 
@@ -148,5 +201,26 @@ function increaseNumber() {
 .widthofcon {
     width: 100vw;
     max-width: 100vw;
+}
+
+.setheight {
+    min-height: 100px;
+}
+
+.bars{
+    padding: 0 2em;
+}
+
+
+.spaceuper {
+    min-height: 500px;
+}
+
+.parallelogram {
+    transform: skew(16deg);
+}
+
+.parallelogram > div {
+    transform: skew(-16deg);
 }
 </style>
