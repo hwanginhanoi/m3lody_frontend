448e54fe985e8791cbac14d600f837661a58b8b2
khanhdnk <dangnamkhanhkdgv@gmail.com>
2024-03-06 23:20:03 +0700

    remove some unecessary padding

--- main/src/views/Api.vue
+++ main/src/views/Api.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
 // Importing necessary functions from Vue
 import {ref} from "vue";
-
+import {onMounted} from "vue";
 // Creating a reactive reference for the tab
 import getUsersData from "../apis/getUsersData";
-let users = await getUsersData();
-console.log(users);
+const users = ref([]);
+// onMounted(async () => {
+// });
+async function handleClick() {
+    try {
+        // Fetch users data
+        users.value = await getUsersData();
+        // Update the users array with fetched data
+    } catch (error) {
+        console.error("Error fetching users data:", error);
+    }
+
+}
+
+defineExpose({
+    handleClick
+});
 </script>
 
 <template>
     <v-main>
-        <table>
+        <Button class="border px-1 py-2 ma-2 rounded-lg bg-green-accent-2" @click="handleClick">Click me</Button>
+        <table border="1">
             <thead>
                 <tr>
                     <th>user_id</th>
