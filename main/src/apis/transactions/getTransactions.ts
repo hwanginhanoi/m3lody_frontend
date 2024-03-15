8005728a0f9b176c7685a0b23756babc2253b5b2
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-03-15 22:18:50 +0700

    u

---
+++ main/src/apis/transactions/getTransactions.ts
@@ -0,0 +1,27 @@
+// import axios from 'axios';
+import responseData from '../interfaces/responseData';
+async function getTransactions(){
+    let url = "http://localhost:3001/transactions";
+    try{
+        const response = await fetch(url,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const responseFromServer: responseData = await response.json();
+        if (!responseFromServer.success){
+            throw new Error("Can't retrieve data");
+        }
+        return responseFromServer.data;
+
+    }catch(error){
+    }
+}
+
+export default getTransactions;
