595ce8125b6f0264e5b74b8b9eb4b288198538f5
Luu Tuan Hoang <hoanglt0104@gmail.com>
2024-03-21 19:29:10 +0700

    Update getUsersData.ts

--- main/src/apis/getUsersData.ts
+++ main/src/apis/getUsersData.ts
@@ -1,26 +1,27 @@
 // import axios from 'axios';
 import responseData from '../interfaces/responseData';
-async function getUsersData(){
+
+async function getUsersData() {
     let url = "http://localhost:3001/users";
-    try{
-        const response = await fetch(url,{
+    try {
+        const response = await fetch(url, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             }
-            
+
         });
 
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const responseFromServer: responseData = await response.json();
-        if (!responseFromServer.success){
+        if (!responseFromServer.success) {
             throw new Error("Can't retrieve data");
         }
         return responseFromServer.data;
-        
-    }catch(error){
+
+    } catch (error) {
     }
 }
 
